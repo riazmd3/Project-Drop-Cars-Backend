f8171f26fc1f58a527cef7e67c7249c274e5d32e
Pugazheshwar <pugazheshwar411@gmail.com>
2025-08-16 10:50:28 +0530

    Created models for vendor

---
+++ app/models/vendor.py
@@ -0,0 +1,26 @@
+# models/vehicle_owner.py
+from sqlalchemy import Column, String, TIMESTAMP, Integer, func, Boolean, Enum as SqlEnum
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import enum
+from app.database.session import Base
+
+class AccountStatusEnum(enum.Enum):
+    ACTIVE = "Active"
+    INACTIVE = "Inactive"
+    PENDING = "Pending"
+    
+class VehicleOwnerCredentials(Base):
+    __tablename__ = "vendor"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
+    organization_id = Column(String)
+    primary_number = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    account_status = Column(
+        SqlEnum(AccountStatusEnum, name="account_status_enum"),
+        default=AccountStatusEnum.INACTIVE,
+        nullable=False
+    )
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
+    
