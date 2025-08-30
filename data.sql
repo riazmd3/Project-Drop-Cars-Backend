46ff1d6f84cd4a18b0c6afbd81dadbb12650feee
Pugazheshwar <pugazheshwar411@gmail.com>
2025-08-30 17:57:00 +0530

    SQL file to delete the tables

--- data.sql
+++ data.sql
@@ -1,21 +1,9 @@
--- data.sql
-
-INSERT INTO admin (
-    id,
-    username,
-    password,
-    role,
-    email,
-    phone,
-    organization_id,
-    created_at
-) VALUES (
-    'a7f9f59e-1d4b-4bfa-9b57-3a8c0e4d4a11',
-    'adminuser',
-    'securepassword123',
-    'Owner',
-    'admin@example.com',
-    '9876543210',
-    '9cbd3e60-476a-4b1d-a81a-938e19ad15b1',
-    NOW()
-);
+DROP TABLE IF EXISTS "drop-cars"."car_details" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."vendor_details" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."car_driver" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."new_orders" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."vehicle_owner" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."vehicle_owner_details" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."admin" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."order_assignments" CASCADE;
+DROP TABLE IF EXISTS "drop-cars"."vendor" CASCADE;
